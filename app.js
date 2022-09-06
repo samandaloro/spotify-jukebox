cc3eed6b3532d80d1f8f3feb8d0f1bf75adbdabd
Sam Andaloro <sjandaloro@gmail.com>
2022-09-06 17:41:37 -0400

    Add New Playlist Functionality Addition

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 /* Spotify playlist IDs -> can be retrieved from the playlist url on open.spotify.com/playlist/ */
 var playlistIds = ["37i9dQZF1DXa8NOEUWPn9W", "37i9dQZF1DXa2PvUpywmrr", "37i9dQZF1DX4dyzvuaRJ0n", "37i9dQZF1DWVY4eLfA3XFQ", 
                    "37i9dQZF1DXcBWIGoYBM5M", "37i9dQZF1DX4JAvHpjipBk", "37i9dQZF1DX0BcQWzuB7ZO", "37i9dQZF1DX1lVhptIYRda",
-                   "37i9dQZF1DX3oM43CtKnRV", "37i9dQZF1DWVqJMsgEN0F4", "37i9dQZF1DWUZv12GM5cFk", "37i9dQZF1DXd1MXcE8WTXq",];
+                   "37i9dQZF1DX3oM43CtKnRV", "37i9dQZF1DWVqJMsgEN0F4", "37i9dQZF1DWUZv12GM5cFk", "37i9dQZF1DXd1MXcE8WTXq"];
+var originalPlaylistSize = playlistIds.length;
 /* Playlist API */
 var playlistAPI = "https://api.spotify.com/v1/playlists/";
 
@@ -90,72 +91,90 @@ async function getPlaylists() {
         isTokenValid = true;
         getPlaylists();
     }
-    else { createPlaylist(); }
+    else {
+        createDefaultPlaylists(); 
+    }
     
 }
 
-
-function createPlaylist() {
-
+function createDefaultPlaylists() {
     for(i=0; i<playlists.length; i++) {
+        createPlaylist(i);
+    }
+}
 
-        /* Get current slide div */
-        var currentSlide = document.getElementById("playlist"+i);
+function createPlaylist(i) {
 
-        /* Create and style div container for the playlist */
-        var element = document.createElement("div");
-        styleDivElement(element);
-
-        /* Create and style image and link element */
-        var imageLink = document.createElement("a");
-        styleImage(imageLink);
-
-        /* Create and style playlist title */
-        var titleElement = document.createElement("h1");
-        styleTitle(titleElement);
-
-        /* Create and style playlist owner */
-        var ownerElement = document.createElement("h5");
-        styleOwner(ownerElement);
-
-        /* Create and style playlist description */
-        var descriptionElement = document.createElement("p");
-        styleDescription(descriptionElement);
-
-        /* Create and style playlist top tracks heading */
-        var topTracks = document.createElement("p");
-        styleTopTracks(topTracks);
-
-        /* Create and style playlist top tracks list */
-        var topTracksList = document.createElement("h4");
-        styleTopTracksList(topTracksList);
-
-        /* Create and style playlist followers */
-        var followersElement = document.createElement("p");
-        styleFollowers(followersElement);
-        
-        /* Append all children to the slide element */
-        element.appendChild(imageLink);
-        element.appendChild(titleElement);
-        element.appendChild(ownerElement);
-        element.appendChild(descriptionElement);
-        element.appendChild(document.createElement("br"));
-        element.appendChild(topTracks);
-        element.appendChild(topTracksList)
-        element.appendChild(followersElement);
-
-        /* Append to the current slide */
-        currentSlide.appendChild(element);
+    var currentSlide;
+    /* Check if playlist is an addition */
+    if (i > originalPlaylistSize - 1) {
+        var newSlide = document.createElement("div");
+        newSlide.className = "slide fade";
+        newSlide.style.display = "none";
+        currentSlide = document.createElement("div");
+        currentSlide.className = "playlist"+i;
+        var slideshowElement = document.getElementById("slideshow");
+        newSlide.appendChild(currentSlide);
+        slideshowElement.appendChild(newSlide);
 
     }
+    else {
+        /* Get default slide div */
+        var currentSlide = document.getElementById("playlist"+i);
+    }
+
+    /* Create and style div container for the playlist */
+    var element = document.createElement("div");
+    styleDivElement(element, i);
+
+    /* Create and style image and link element */
+    var imageLink = document.createElement("a");
+    styleImage(imageLink, i);
+
+    /* Create and style playlist title */
+    var titleElement = document.createElement("h1");
+    styleTitle(titleElement, i);
+
+    /* Create and style playlist owner */
+    var ownerElement = document.createElement("h5");
+    styleOwner(ownerElement, i);
+
+    /* Create and style playlist description */
+    var descriptionElement = document.createElement("p");
+    styleDescription(descriptionElement, i);
+
+    /* Create and style playlist top tracks heading */
+    var topTracks = document.createElement("p");
+    styleTopTracks(topTracks, i);
+
+    /* Create and style playlist top tracks list */
+    var topTracksList = document.createElement("h4");
+    styleTopTracksList(topTracksList, i);
+
+    /* Create and style playlist followers */
+    var followersElement = document.createElement("p");
+    styleFollowers(followersElement, i);
+        
+    /* Append all children to the slide element */
+    element.appendChild(imageLink);
+    element.appendChild(titleElement);
+    element.appendChild(ownerElement);
+    element.appendChild(descriptionElement);
+    element.appendChild(document.createElement("br"));
+    element.appendChild(topTracks);
+    element.appendChild(topTracksList)
+    element.appendChild(followersElement);
+
+    /* Append to the current slide */
+    currentSlide.appendChild(element);
 
 }
 
-function styleDivElement(element) {
+function styleDivElement(element, i) {
     element.className= "slide";
 }
 
-function styleImage(imageLink) {
+function styleImage(imageLink, i) {
     imageLink.setAttribute("href", playlists[i].URI);
     var imageElement = document.createElement("img")
     imageElement.className = "img";
@@ -164,32 +183,32 @@ function styleImage(imageLink) {
     imageLink.appendChild(imageElement);  
 }
 
-function styleTitle(titleElement) {
+function styleTitle(titleElement, i) {
     titleElement.className = "title"
     titleElement.innerHTML = playlists[i].Name+" ";
 }
 
-function styleDescription(descriptionElement) {
+function styleDescription(descriptionElement, i) {
     descriptionElement.className = "description";
     descriptionElement.innerHTML = removeCover(playlists[i].Description);
 }
 
-function styleFollowers(followersElement) {
+function styleFollowers(followersElement, i) {
     followersElement.className = "followers";
     followersElement.innerHTML = playlists[i].Followers + " followers"
 }
 
-function styleOwner(ownerElement) {
+function styleOwner(ownerElement, i) {
     ownerElement.className = "owner";
     ownerElement.innerHTML = " by " + playlists[i].Owner;
 }
 
-function styleTopTracks(topTracks) {
+function styleTopTracks(topTracks, i) {
     topTracks.className = "top";
     topTracks.innerHTML = "Top Tracks";
 }
 
-function styleTopTracksList(topTracksList) {
+function styleTopTracksList(topTracksList, i) {
     for(j=0; j<trackAmount; j++) {
         var track = document.createElement("p");
         track.className = "track";
@@ -206,4 +225,21 @@ function styleTopTracksList(topTracksList) {
 function removeCover(name) {
     if (name.search("Cover:") != -1) { return name.substring(0, name.search("Cover:")); }
     return name;
+}
+
+/* Functionality to add a new playlist */
+async function addNewPlaylist() {
+    var pid = document.getElementById("newPlaylist").value;
+    document.getElementById("newPlaylist").value = "";
+    console.log("New playlist id " + pid);
+    if (pid !== null) {
+        playlists.push(await fetchPlaylistInfo(pid));
+        if (isTokenValid == false) { 
+            token = prompt(errMsg + " and provide new token here: ");
+            playlists = [];
+            isTokenValid = true;
+        }
+    createPlaylist(playlists.length-1);
+    console.log(playlists[playlists.length-1].Name+ " added")
+    }
 }
